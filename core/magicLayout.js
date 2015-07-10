254790fbeaf2f3f02f2259029741774807c6e79d
Stas <melnik909@ya.ru>
2015-07-10 14:49:08 +0300

    привел к единому стилю код

--- core/magicLayout.js
+++ core/magicLayout.js
@@ -6,20 +6,21 @@ github: https://github.com/melnik909
 facebook: https://www.facebook.com/melnik909
 */
 
-(function($) { 
+(function($){ 
 
 	var methods = {
-		init: function (options){
+		
+		init: function(options){
 
 		   return this.each(function(){
 
-				if(!options.container.hasClass('active_container')){
+				if(!options.container.hasClass("active_container")){
 			
-					options.container.find('.layer').map(function(){
+					options.container.find(".layer").map(function(){
 						var show_effect = $(this).attr("data-show-effect"),
 						time = $(this).attr("data-time"),
 						delay = $(this).attr("data-show-delay");
-						$(this).addClass('animated ' + show_effect).css({'animation-delay' : delay + 'ms',  'animation-duration' : time + 'ms'});
+						$(this).addClass("animated " + show_effect).css({'animation-delay' : delay + 'ms',  'animation-duration' : time + 'ms'});
 
 					});
 				}
@@ -29,13 +30,17 @@ facebook: https://www.facebook.com/melnik909
 
 	$.fn.magicLayout = function(method){
 
-		if ( methods[method] ) {
-		  return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-		  return methods.init.apply( this, arguments );
+		if(methods[method]){
+			
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			
+		}else if(typeof method === 'object' || ! method){
+			
+			return methods.init.apply( this, arguments );
+			
 		} else {
-		  $.error( 'This' +  method + ' is not defined' );
+			
+			$.error( 'This' +  method + ' is not defined' );
 		}
-
 	};
 })(jQuery);
